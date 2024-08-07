35e1c5b94a9e6091bc820ebf5c62b5dd8aee8c6f
Andarooo <andrew.park.j@gmail.com>
2024-08-08 00:08:59 +0800

    Cleaning up repo after I messed it up trying to get pages to work

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    GoogleAPIKey:"AIzaSyAeFHdDs_oj1RGusKGFuo7MO40z4NSn9z0",
+    GoogleAPIKey:"",
     biocacheURL : 'https://biocache.ala.org.au/ws/occurrences/facets',
     traitURL : 'https://bie.ala.org.au/externalSite/ausTraitsSummary',
     speciesURL : 'https://api.ala.org.au/species/species/lookup/bulk',
