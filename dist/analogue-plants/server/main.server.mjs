baa2fb98b755206c1f674e07c91f30cbebff48a2
Andarooo <andrew.park.j@gmail.com>
2024-08-07 23:10:28 +0800

    Another attempt at geting pages to work (who knows what I'm doing here)

--- dist/analogue-plants/server/main.server.mjs
+++ dist/analogue-plants/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-M4NB3DPA.mjs";import"./chunk-EPZE7J2E.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-CER6ABBG.mjs";import"./chunk-EPZE7J2E.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
